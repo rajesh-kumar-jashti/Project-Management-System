df86f9d6c3c320a7b2983bc10fdf8afc5dfdec82
rajesh-kumar-jashti <rajeshkumarjashti@gmail.com>
2024-10-03 23:26:48 +0530

    authentication

---
+++ src/main/java/com/rajeshkumar/config/JwtConstant.java
@@ -0,0 +1,6 @@
+package com.rajeshkumar.config;
+
+public class JwtConstant {
+    public static final String SECRET_KEY = "rajeshkumar";
+    public static final String JWT_HEADER = "Authorization";
+}
---
+++ src/main/java/com/rajeshkumar/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.rajeshkumar.repository;
+
+import com.rajeshkumar.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    User findByEmail(String email);
+}
